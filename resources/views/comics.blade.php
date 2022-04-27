02da52ecdee45a94c0c1cc31fd223b0ec395c5c7
Barbarubia <dnl.desimone@gmail.com>
2022-04-27 18:08:06 +0200

    creata route comics + ciclo foreach su comics

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,15 @@
+@extends('template.base')
+
+@section('title', 'Comics')
+
+@section('content')
+    <h1>Comics</h1>
+    <div class="comics-container">
+        @foreach ($comics as $comic)
+            <div class="comic-card">
+                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                <h3>{{ $comic['series'] }}</h3>
+            </div>
+        @endforeach
+    </div>
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,66 @@
 <header>
     Questo è l'header.
+
+    <div class="header-top">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#">DC Power<sup>SM</sup> Visa <i class="fa-regular fa-registered"></i></a>
+                </li>
+                <li>
+                    <a href="#">Additional DC Sites</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="header-bottom">
+        <div class="container">
+            <div class="logo">
+                <a href="#">
+                    <img src="" alt="Logo">
+                </a>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">Fans</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+    </div>
+
+    Fine header
 </header>
