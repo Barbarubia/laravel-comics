0951869f73630a54f1e0e19aa86c75a7a84f9498
Barbarubia <dnl.desimone@gmail.com>
2022-04-28 10:20:18 +0200

    menu header generato con foreach

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -22,36 +22,15 @@
             </div>
             <div class="menu">
                 <ul>
-                    <li class="{{ Route::current()->getname() == 'characters' ? 'active' : '' }}">
-                        <a href="{{ route('characters') }}">Characters</a>
-                    </li>
-                    <li class="{{ Route::current()->getname() == 'comics' ? 'active' : '' }}">
-                        <a href="{{ route('comics') }}">Comics</a>
-                    </li>
-                    <li class="{{ Route::current()->getname() == 'movies' ? 'active' : '' }}">
-                        <a href="{{ route('movies') }}">Movies</a>
-                    </li>
-                    <li class="{{ Route::current()->getname() == 'tv' ? 'active' : '' }}">
-                        <a href="{{ route('tv') }}">TV</a>
-                    </li>
-                    <li class="{{ Route::current()->getname() == 'games' ? 'active' : '' }}">
-                        <a href="{{ route('games') }}">Games</a>
-                    </li>
-                    <li class="{{ Route::current()->getname() == 'collectibles' ? 'active' : '' }}">
-                        <a href="{{ route('collectibles') }}">Collectibles</a>
-                    </li>
-                    <li class="{{ Route::current()->getname() == 'videos' ? 'active' : '' }}">
-                        <a href="{{ route('videos') }}">Videos</a>
-                    </li>
-                    <li class="{{ Route::current()->getname() == 'fans' ? 'active' : '' }}">
-                        <a href="{{ route('fans') }}">Fans</a>
-                    </li>
-                    <li class="{{ Route::current()->getname() == 'news' ? 'active' : '' }}">
-                        <a href="{{ route('news') }}">News</a>
-                    </li>
-                    <li class="{{ Route::current()->getname() == 'shop' ? 'active' : '' }}">
-                        <a href="{{ route('shop') }}">Shop</a>
-                    </li>
+                    @php
+                        $headermenu = config('headermenu')
+                    @endphp
+
+                    @foreach ($headermenu as $menuitem)
+                        <li class="{{ Route::currentRouteName() == $menuitem['route'] ? 'active' : '' }}">
+                            <a href="{{ route($menuitem['route']) }}">{{ $menuitem['route'] }}</a>
+                        </li>
+                    @endforeach
                 </ul>
             </div>
             <div class="search">
